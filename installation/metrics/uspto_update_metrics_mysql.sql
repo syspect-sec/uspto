901dc79fd0ffdc588f95d59eab39616f2f51bc0b
rippledj <joseph.lee.esl@gmail.com>
2020-10-25 15:45:01 -0400

    Added FamilySize to METRICS_G

--- installation/metrics/uspto_update_metrics_mysql.sql
+++ installation/metrics/uspto_update_metrics_mysql.sql
@@ -24,3 +24,39 @@ WHERE  uspto.METRICS_G.GrantID = t2.GrantID;
 UPDATE uspto.METRICS_G
 SET uspto.METRICS_G.BackwardCitCnt = 0
 WHERE uspto.METRICS_G.BackwardCitCnt IS NULL;
+
+--
+-- Combines the CONTINUITYCHILD_P and CONTINUITYPARENT_P
+-- tables and provdies a count of unique country codes that
+-- an application has been filed in.
+--
+UPDATE METRICS_G m
+JOIN (
+  SELECT
+  ApplicationID,
+   COUNT( DISTINCT
+   CASE
+   WHEN `RefID` REGEXP '^[0-9]+$' THEN 'US'
+   ELSE SUBSTRING(`RefID`, 5, 2)
+   END
+   ) FamilySize,
+   GrantID
+  FROM (
+    SELECT g.ApplicationID, ChildApplicationID `RefID`, g.GrantID
+    FROM CONTINUITYCHILD_P `child`
+    JOIN uspto.GRANT as g ON child.ApplicationID = g.ApplicationID
+    UNION
+    SELECT g2.ApplicationID, ParentApplicationID `RefID`, g2.GrantID
+    FROM CONTINUITYPARENT_P `par`
+    JOIN uspto.GRANT as g2 ON par.ApplicationID = g2.ApplicationID
+  ) t1
+  GROUP BY ApplicationID
+) t2 ON m.GrantID = t2.GrantID
+SET m.FamilySize = t2.FamilySize
+
+--
+-- Set all patents without FamilySize to 0
+--
+UPDATE uspto.METRICS_G
+SET uspto.METRICS_G.FamilySize = 0
+WHERE uspto.METRICS_G.FamilySize IS NULL;
